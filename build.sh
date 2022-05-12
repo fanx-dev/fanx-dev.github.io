1052ceab9aad95be28da5d8d8750370eff48a4c9
chunquedong <chunquedong@163.com>
2022-05-12 21:24:34 +0800

    update api doc

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 
-fan compilerDoc -all
-cp -r ../fanx/env/doc/* apidoc/
+FANX_DIR=../fanx/release/fanx-4.2.0
+
+${FANX_DIR}/bin/fan compilerDoc -all
+cp -r ${FANX_DIR}/doc/* apidoc/
 
 cd src
 fan build.fan
